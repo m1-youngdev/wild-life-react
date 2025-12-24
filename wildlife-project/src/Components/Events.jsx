71bb7dc8aa6a7a972b871461b23b7ebfbcb35ca2
Marshal John <marshaljohn2006@gmail.com>
2025-12-24 10:36:38 +0100

    Card hover effects

--- wildlife-project/src/Components/Events.jsx
+++ wildlife-project/src/Components/Events.jsx
@@ -8,17 +8,17 @@ function Events() {
         </div>
         <div className='max-w-screen flex justify-center items-center'>
             <div className='cards grid grid-cols-3 w-9/12 gap-5 py-10'>
-                <div className="card rounded-lg shadow-[#71AC8b] shadow-md p-5">
+                <div className="card rounded-lg shadow-[#71AC8b] shadow-md p-5 hover:scale-105 transition ease-in-out duration-700">
                     <h2 className='font-montserrat font-semibold text-2xl mb-3'>Saving the Endangered Rhino</h2>
                     <p className="font-montserrat mb-3 text-[15px]">Explore the journey and challenges faced in protecting one of the most endangered species on Earth.</p>
                     <a href='#' className='font-montserrat text-[16px]'>Read more</a>
                 </div>
-                <div className="card rounded-lg shadow-[#71AC8b] shadow-md p-5">
+                <div className="card rounded-lg shadow-[#71AC8b] shadow-md p-5 hover:scale-105 transition ease-in-out duration-700">
                     <h2 className='font-montserrat font-semibold text-2xl mb-3'>The Journey of Migrating Elephants</h2>
                     <p className="font-montserrat mb-3 text-[15px]">Learn about the incredible migration of elephants across Africa.</p>
                     <a href='#' className='font-montserrat text-[16px]'>Read more</a>
                 </div>
-                <div className="card rounded-lg shadow-[#71AC8b] shadow-md p-5">
+                <div className="card rounded-lg shadow-[#71AC8b] shadow-md p-5 hover:scale-105 transition ease-in-out duration-700">
                     <h2 className='font-montserrat font-semibold text-2xl mb-3'>The Secret Life of Tigers</h2>
                     <p className="font-montserrat mb-3 text-[15px]">An in-depth look into the behaviors and habits of wild tigers</p>
                     <a href='#' className='font-montserrat text-[16px]'>Read more</a>
